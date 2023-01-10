e261b4aaef5d2f2a06334afe38c480a6c3958759
Atif K <s107214@ap.be>
2023-01-10 22:23:13 +0100

    Added contact page with basic info and CSS

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,18 @@
-const ContactPage = () => {
+import Layout from "../components/layout";
+import React from "react";
+import {container} from "./contact.module.css"
 
+const ContactPage = () => {
+    return (
+        <Layout>
+            <div className={container}>
+                <h1>CONTACT US</h1>
+                <h2>Email: msi@laptops.com</h2>
+                <h2>Number: 0409892909</h2>
+                <h2>Adress: Laptoplei 182, 2060 Antwerpen</h2>
+            </div>
+        </Layout>
+    )
 }
 
 export default ContactPage;

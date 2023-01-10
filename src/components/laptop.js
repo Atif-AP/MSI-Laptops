61591c5dea79d941d761b691a76e5ffd6ec79dd9
Atif K <s107214@ap.be>
2023-01-10 20:28:12 +0100

    Finished Home page css (for now)

--- src/components/laptop.js
+++ src/components/laptop.js
@@ -1,13 +1,13 @@
 import { Link } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
-import {title, link} from "./laptop.module.css"
+import {title, link, img} from "./laptop.module.css"
 
 const Laptop = ({laptop, slug}) => {
     const profile = getImage(laptop.msiLaptopsFields.picture.localFile)
     return (
         <Link to={slug} className={link}>
-            <GatsbyImage image={profile} alt={laptop.msiLaptopsFields.picture.altText} />
+            <GatsbyImage className={img} image={profile} alt={laptop.msiLaptopsFields.picture.altText} />
             <div className={title}>
                 {laptop.msiLaptopsFields.title}
             </div>

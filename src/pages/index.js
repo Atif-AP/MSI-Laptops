4ec60f5d87c07abcfe6b8a30cba7e81c90793138
Atif-AP <s107214@ap.be>
2023-01-10 13:51:50 +0100

    Initialized gatsby en first query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
   return (
     <main>
-      <Layout pageTitle="Welcome to Inghelbrecht Agency!">
+      <Layout pageTitle={data.site.siteMetadata.title}>
       <p>Lorem ipsum</p>
       <StaticImage
         alt="randomized unsplash image!"

7a0dd25500ca0e802ed801e234f06ac48f97206c
Atif K <s107214@ap.be>
2023-01-10 22:34:12 +0100

    Created Footer component

---
+++ src/components/footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = ({ siteTitle, companyInfo}) => {
+    <footer>
+        <div>
+            <p>{siteTitle}</p>
+        </div>
+        <div>
+            <p>{`${companyInfo.address}, ${companyInfo.zipCode} ${companyInfo.city}`}</p>
+            <div>
+                Follow us:
+                <a
+                    target="__blank"
+                    href={companyInfo.facebook}
+                />
+                <a
+                    target="__blank"
+                    href={companyInfo.instagram}
+                />
+            </div>
+        </div>
+    </footer>
+}
+
+export default Footer;

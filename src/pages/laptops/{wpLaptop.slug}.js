54903b65ce3edb846a8664e634a0e54297e41649
Atif K <s107214@ap.be>
2023-01-10 17:51:16 +0100

    Added immages to laptops

--- src/pages/laptops/{wpLaptop.slug}.js
+++ src/pages/laptops/{wpLaptop.slug}.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Layout from "../../components/layout";
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { graphql } from 'gatsby'
 
 export const query = graphql`
@@ -15,16 +16,26 @@ query ($slug: String) {
       screenSize
       title
       typeOfStorage
+      picture {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
+        altText
+      }
     }
   }
 }
 `
 
 const LaptopsPage = ({data: {wpLaptop: {msiLaptopsFields: laptop}}}) => {
+  const image = getImage(laptop.picture.localFile)
 
   return(
     <Layout pageTitle="Laptops Template">
         <div>
+          <GatsbyImage image={image} alt={laptop.picture.altText}/>
           <h1>{laptop.title}</h1>
           <div dangerouslySetInnerHTML={{__html: laptop.description}}/>
           <p>Processor: {laptop.processorType}</p>

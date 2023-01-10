60ee694d6968e374959c6a084cacf42b0a1f028f
Atif K <s107214@ap.be>
2023-01-10 23:42:38 +0100

    added a contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import Layout from "../components/layout";
 import React from "react";
-import {container} from "./contact.module.css"
+import {container, form} from "./contact.module.css"
 import { graphql } from "gatsby";
 
 export const query = graphql`
@@ -20,6 +20,18 @@ query {
 const ContactPage = ({data: {wpPage: {contactUsFields: contact}}}) => {
     return (
         <Layout>
+            <div className={form}>
+                <form name="contact" method="POST" data-netlify="true">
+                    <label>Your name:</label>
+                    <input type="text" name="name" required={true}></input>
+                    <label>Your Email:</label>
+                    <input type="email" name="email" required={true}></input>
+                    <label>Message:</label>
+                    <textarea name="message" required={true}></textarea>
+                    <input type="hidden" name="form-name" value="contact"></input>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
             <div className={container}>
                 <h1>CONTACT US</h1>
                 <h2>Email: msi@laptops.com</h2>

a6e067373eaadf3e36cb2648d944f6195878158e
Atif K <s107214@ap.be>
2023-01-10 23:35:12 +0100

    added CSS to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {container,site,info} from "./footer.module.css"
+import {container,site,info,facebook,instagram} from "./footer.module.css"
 
 const Footer = ({ siteTitle, companyInfo}) => {
     return (
@@ -12,10 +12,12 @@ const Footer = ({ siteTitle, companyInfo}) => {
                 <div>
                     Follow us:
                     <a
+                        className={facebook}
                         target="__blank"
                         href={companyInfo.facebook}
                     />
                     <a
+                        className={instagram}
                         target="__blank"
                         href={companyInfo.instagram}
                     />

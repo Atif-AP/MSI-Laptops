7cc86d47fe06c487e3e1aaa5086650e524f63433
Atif K <s107214@ap.be>
2023-01-10 22:55:50 +0100

    Added query to contact page and bug fixes footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import { graphql, Link, useStaticQuery } from 'gatsby'
 import { header, nav, navLinks, navLinkItem, navLinkText, title } from './layout.module.css'
+import Footer from './footer'
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -10,42 +11,57 @@ const Layout = ({ pageTitle, children }) => {
           title
         }
       }
+      wpPage(slug: {eq: "contact-us"}) {
+        contactUsFields {
+          address
+          city
+          facebook
+          instagram
+          zipcode
+        }
+      }
     }
   `)
   return (
-    <div>
-      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <div className={header}>
-        <h1 className={title}>{data.site.siteMetadata.title}</h1>
-        <nav className={nav}>
-          <ul className={navLinks}>
-            <li className={navLinkItem}>
-              <Link className={navLinkText} to="/">
-                Home
-              </Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link className={navLinkText} to="/about">
-                About
-              </Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link className={navLinkText} to="/laptops">
-                Laptops
-              </Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link className={navLinkText} to="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
+    <>
+      <div>
+        <title>{data.site.siteMetadata.title}</title>
+        <div className={header}>
+          <h1 className={title}>{data.site.siteMetadata.title}</h1>
+          <nav className={nav}>
+            <ul className={navLinks}>
+              <li className={navLinkItem}>
+                <Link className={navLinkText} to="/">
+                  Home
+                </Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link className={navLinkText} to="/about">
+                  About
+                </Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link className={navLinkText} to="/laptops">
+                  Laptops
+                </Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link className={navLinkText} to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <main>
+          {children}
+        </main>
       </div>
-      <main>
-        {children}
-      </main>
-    </div>
+      <Footer 
+        siteTitle={data.site.siteMetadata.title} 
+        companyInfo={data.wpPage.contactUsFields}
+      />
+    </>
   )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,30 @@
 import Layout from "../components/layout";
 import React from "react";
 import {container} from "./contact.module.css"
+import { graphql } from "gatsby";
 
-const ContactPage = () => {
+export const query = graphql`
+query {
+    wpPage(slug: {eq: "contact-us"}) {
+      contactUsFields {
+        address
+        city
+        facebook
+        instagram
+        zipcode
+      }
+    }
+  }
+`
+
+const ContactPage = ({data: {wpPage: {contactUsFields: contact}}}) => {
     return (
         <Layout>
             <div className={container}>
                 <h1>CONTACT US</h1>
                 <h2>Email: msi@laptops.com</h2>
                 <h2>Number: 0409892909</h2>
-                <h2>Adress: Laptoplei 182, 2060 Antwerpen</h2>
+                <h2>Address: {contact.address}, {contact.zipcode} {contact.city}</h2>
             </div>
         </Layout>
     )
